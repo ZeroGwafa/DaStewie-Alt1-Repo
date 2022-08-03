f1fccc983452ed5c3c830ab74fca767b2f479183
s2610574 <s.n.wijnja@student.utwente.nl>
2022-08-04 01:04:51 +0200

    Added solution for medium/large icons

--- bolg/src/index.ts
+++ bolg/src/index.ts
@@ -2,10 +2,6 @@
 //also gives your editor info about the window.alt1 api
 import * as A1lib from "@alt1/base";
 import * as BuffReader from "@alt1/buffs";
-import * as OCR from "@alt1/ocr";
-import { Canvas } from "canvas";
-
-import * as $ from "./jquery";
 
 var font = require("@alt1/ocr/fonts/pixel_digits_8px_shadow.js");
 
@@ -17,13 +13,44 @@ var imgs = A1lib.ImageDetect.webpackImages(
     {
         "bolg": require("./images/bolg.data.png"),
         "bolg_large": require("./images/bolg_large.data.png"),
+        "bolg_small": require("./images/bolg_small.data.png"),
+        "bolg_medium": require("./images/bolg_medium.data.png"),
+        "bolg_larg": require("./images/bolg_larg.data.png"),
 	}
 );
 
+// Search for bolg buff by using images. This is a slow method but does support medium and large buffs.
+var buff_sizes = [ "bolg_small", "bolg_medium", "bolg_larg" ];
+function readBuffsByImage() {
+    let canvas = document.getElementById("canvas") as HTMLCanvasElement;
+    let ctx = canvas.getContext("2d");
+    ctx.drawImage(imgs.bolg_large.toImage(), 0, 0, canvas.width, canvas.height);   
+
+    var img = A1lib.captureHoldFullRs();
+    
+    var sizes = {
+        "bolg_small": 27, 
+        "bolg_medium": 32, 
+        "bolg_larg": 36
+    }
+
+    // Look for the current phase
+    for (let key in buff_sizes) {
+        var name = buff_sizes[key];
+        var img_found = img.findSubimage(imgs[name]);
+        if (img_found.length > 0) { 
+            var size = sizes[name];
+            var buff = A1lib.capture(img_found[0].x, img_found[0].y, size, size);
+            
+            ctx.drawImage(buff.toDrawableData().toImage(), 0, 0, size, size, 0, 0, canvas.width, canvas.height);
+            buff_sizes = [ buff_sizes[key] ];
+            break;
+        }
+    }
+}
+
 
 let reader = new BuffReader.default();
-
-
 function showSelectedBuffs(chat) {
     var buffsize = 27;
 	var gridsize = 30;
@@ -42,37 +69,6 @@ function showSelectedBuffs(chat) {
     } catch { }
 }
 
-//Find all visible chatboxes on screen
-let findChat = setInterval(function () {
-    if (reader.pos === null)
-        reader.find();
-    else {
-        clearInterval(findChat);
-        var square = reader.getCaptRect();
-        showSelectedBuffs(square);
-
-        setInterval(function () {
-            readBuffs();
-        }, 100);
-    }
-}, 1000);
-
-function readBuff(buffer: ImageData, ox: number, oy: number) {
-    var lines: string[] = [];
-    // For numbers without brackets dx, dy = [2, 10]
-    // For numbers with brackets    dx, dy = [1, 20]
-
-    for (var dx = 1; dx <= 2; dx++) {
-        for (var dy = 10; dy <= 20; dy += 10) {
-            var result = OCR.readLine(buffer, font, [255, 255, 255], ox + dx, oy + dy, true);
-            if (result.text) { 
-                lines.push(result.text); 
-            }
-        }
-    } 
-    return lines;
-}
-
 function readBuffs() {
     var opts = reader.read() || [];
     let canvas = document.getElementById("canvas") as HTMLCanvasElement;
@@ -89,7 +85,48 @@ function readBuffs() {
     }
 }
 
-//check if we are running inside alt1 by checking if the alt1 global exists
+
+document.getElementById("canvas").onclick = function () {
+    // Toggle the image search setting
+    if (localStorage["bolg_search"] == "true") {
+        localStorage["bolg_search"] = "false";
+    } else {
+        localStorage["bolg_search"] = "true";
+    }
+
+    console.log("Using image search: " + localStorage["bolg_search"]);
+    alt1.setTooltip("Using image search: " + localStorage["bolg_search"]);
+    setTimeout(function () {
+        window.location.reload();
+    }, 1000);
+}
+
+// check if we are running inside alt1 by checking if the alt1 global exists
 if (window.alt1) {
     alt1.identifyAppUrl("./appconfig.json");
+
+    if (localStorage["bolg_search"] == "true") {
+        setInterval(function () {
+            readBuffsByImage();
+        }, 100);
+    } else {
+        let findBuffBar = setInterval(function () {
+            if (reader.pos === null)
+                reader.find();
+            else {
+                clearInterval(findBuffBar);
+                var square = reader.getCaptRect();
+                showSelectedBuffs(square);
+        
+                setInterval(function () {
+                    readBuffs();
+                }, 100);
+            }
+        }, 1000);
+    }
+
+    if (!localStorage["bolg_search"]) {
+        // Do not use the inefficient method that supports multiple sizes by default
+        localStorage["bolg_search"] = "false";	
+    }
 }
